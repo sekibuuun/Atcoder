326fe9fdfcaca79f214ad998af189975b2bc2d4b
Aoyama Kazuki <sekibun3109@gmail.com>
2023-12-24 16:10:29 +0900

    A問題

---
+++ 334/A.py
@@ -0,0 +1,13 @@
+str = input().split()
+
+B = int(str[0])
+
+G = int(str[1])
+
+if B > G:
+    print("Bat")
+else: 
+    print("Glove")
+
+# B,G = map(int,input().split())
+# {print("Bat") if B>G else print("Glove")}
